93a4d2ef5ba153f5e817b0e61004b82ee6001437
IDKThatGuy <63104021+IDKThatGuy@users.noreply.github.com>
2021-05-31 23:32:09 +0100

    Update config.lua

--- vrp_dealership/config.lua
+++ vrp_dealership/config.lua
@@ -5,14 +5,26 @@ cfg.Locations = {
     {vector3(-221.16773986816,-1828.2349853516,30.035079956055)}
 }
 
-
+-- ADD YOUR OWN CARS HERE
 cfg.cars = {
-    {spawncode = "t20", vehname = "T20", description = "10KG", price = 7000, category = "Super"},
+    {spawncode = "t20", vehname = "T20", description = "10KG", price = 7000, category = "Super"}, --- EXAMPLE
 
 }
 
 cfg.category = {
-    {title = "Super", name = "Super"}
+    {title = "compacts", name = "compacts"},
+    {title = "coupes", name = "coupes"},
+    {title = "sedans", name = "sedans"},
+    {title = "Super", name = "Super"},
+    {title = "sportsclassics", name = "sportsclassics"},
+    {title = "sports", name = "sports"},
+    {title = "offroad", name = "offroad"},
+    {title = "suvs", name = "suvs"},
+    {title = "vans", name = "vans"},
+    {title = "motorcycles", name = "motorcycles"},
+
+
+
 
     --{title = "WHAT SHOWS ON THE MENU", name = "the category"}
 
